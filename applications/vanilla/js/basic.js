75b95d6e543f39ec85aa97dc2273730d54810e4e
keyreal174 <keyreal174@gmail.com>
2021-02-22 11:58:25 -0500

    [NAF-35] add infinite scroll

--- applications/vanilla/js/basic.js
+++ applications/vanilla/js/basic.js
@@ -6,3 +6,19 @@ $(window).on("scroll", function () {
         $("header").removeClass("not-transparent");
     }
 });
+
+jQuery(document).ready(function($) {
+    var DataListSelector = '#Content ul.DataList.Comments, ' +
+        'main.page-content ul.DataList.Comments, ' +
+        'main.Content ul.DataList.Comments, ' +
+        '#Content ul.DataList.Discussions, ' +
+        'main.page-content ul.DataList.Discussions, ' +
+        'main.Content ul.DataList.Discussions, ' +
+        '#Content table.DataTable.DiscussionsTable tbody, ' +
+        'main.page-content table.DataTable.DiscussionsTable tbody ' +
+        'main.Content table.DataTable.DiscussionsTable tbody',
+    ContentSelector = '#Content, main.page-content, main.Content';
+
+    $(ContentSelector).prepend("<div id='PagerBefore'></div>");
+    $(ContentSelector).append("<div id='PagerAfter'></div>");
+});
