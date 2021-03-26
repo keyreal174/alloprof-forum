d37c6275f665f045491565813e4b5a806a8cc3e9
keyreal174 <keyreal174@gmail.com>
2021-03-26 03:52:44 -0400

    [NAF-106] fix issue in counting when the user is student

--- applications/vanilla/controllers/class.categoriescontroller.php
+++ applications/vanilla/controllers/class.categoriescontroller.php
@@ -524,7 +524,7 @@ class CategoriesController extends VanillaController {
             $cWheres = [];
             $cID = val('CategoryID', $category);
 
-            if ($this->getUserRole() == 'Student') {
+            if ($this->getUserRole() == 'member') {
                 $cWheres = ['d.Published' => 1, 'cm.Published' => 1, 'd.CategoryID' => $cID];
                 $dWheres = ['d.CategoryID' => $cID, 'd.Published' => 1];
             } else {
