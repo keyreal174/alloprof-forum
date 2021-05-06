fd801a1275a356696403f3c4ffdb855d073b0bc3
keyreal174 <keyreal174@gmail.com>
2021-05-06 05:45:11 +0000

    fix URL

--- applications/dashboard/views/search/results.php
+++ applications/dashboard/views/search/results.php
@@ -8,7 +8,7 @@ if (!function_exists('writeDiscussionFooter')) {
 <?php
 if (!count($this->data('SearchResults')) && $this->data('SearchTerm')){
         echo '<p class="NoResults d-desktop">',
-            '<a class="back-btn" href="/discussions"><svg width="24" height="15" viewBox="0 0 24 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            '<a class="back-btn" href="'.url("/discussions").'"><svg width="24" height="15" viewBox="0 0 24 15" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M22.5 7.36426H1.5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M7.88642 13.7282L1.52246 7.36426L7.88642 1.0003" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg></a>',
