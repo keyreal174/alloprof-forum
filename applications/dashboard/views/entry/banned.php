b9112f284d5207a35dbcfe776118a7750aadf974
keyreal174 <keyreal174@gmail.com>
2021-06-01 17:12:34 -0400

    [NAF-183] Fix 404 page redirection issue

--- applications/dashboard/views/entry/banned.php
+++ applications/dashboard/views/entry/banned.php
@@ -7,7 +7,7 @@
 
     <div id="Message"><?php echo t('The page you\'re looking for has disappeared!'); ?></div>
 
-    <a href="/discussions" class="btn btn-default btn-shadow"><?php echo t("Return to Home") ?></a>
+    <a href=<?php echo url("/discussions"); ?> class="btn btn-default btn-shadow"><?php echo t("Return to Home") ?></a>
 </div>
 
 <?php if (debug() && $this->data('Trace')): ?>
--- applications/vanilla/views/modules/checkanswer.php
+++ applications/vanilla/views/modules/checkanswer.php
@@ -5,7 +5,7 @@
             <img src="<?= url('/themes/alloprof/design/images/peace.svg') ?>" width="80px" height="80px" />
             <span> <?php echo t("It's perfect!") ?> </span>
         </a>
-        <a href="/discussion/bad" class="OptionsLink Popup FeedbackHelp">
+        <a href=<?php echo url("/discussion/bad"); ?> class="OptionsLink Popup FeedbackHelp">
             <img src="<?= url('/themes/alloprof/design/images/neutre.svg') ?>" width="80px" height="80px" />
             <span> <?php echo t("I need more help") ?> </span>
         </a>
