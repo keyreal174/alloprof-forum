2aac7ee2f9602312c5ce853f0b3c947bdee5ff39
keyreal174 <keyreal174@gmail.com>
2022-01-08 10:41:44 -0800

    [NAF-320] Change header background in teacher role

--- applications/vanilla/views/modules/banner.php
+++ applications/vanilla/views/modules/banner.php
@@ -15,17 +15,23 @@
 </div>
 <?php }
     else {
+        if ($this->bgColor) {
 ?>
-<div class="Frame-banner Banner">
-    <div class="Banner-content General <?php echo $this->additionalClass; ?>">
-        <div class="Banner-content__Container">
-            <p class="Banner-content__description d-desktop">
-                <?php echo t("Welcome to the Help Zone! <br/> Have questions? Find the answers here!"); ?>
-            </p>
-            <p class="Banner-content__description d-mobile">
-                <?php echo t("Have questions?<br/> Find the answers here!"); ?>
-            </p>
+<div class="Frame-banner Banner" style="background-color: <?php echo $this->bgColor; ?>">
+    <?php
+        } else {
+?>
+    <div class="Frame-banner Banner">
+        <?php }  ?>
+        <div class="Banner-content General <?php echo $this->additionalClass; ?>">
+            <div class="Banner-content__Container">
+                <p class="Banner-content__description d-desktop">
+                    <?php echo t("Welcome to the Help Zone! <br/> Have questions? Find the answers here!"); ?>
+                </p>
+                <p class="Banner-content__description d-mobile">
+                    <?php echo t("Have questions?<br/> Find the answers here!"); ?>
+                </p>
+            </div>
         </div>
     </div>
-</div>
-<?php } ?>
+    <?php } ?>
