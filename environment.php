9537747ad9bd8a1fa2948003563833060bc373ac
keyreal174 <keyreal174@gmail.com>
2022-01-11 04:42:45 +0000

    update APPLICATION_VERSION

--- environment.php
+++ environment.php
@@ -22,7 +22,7 @@ if (!defined('APPLICATION')) {
 if (!defined('APPLICATION_VERSION')) {
     // Rules for the versioning
     // {OSS version}-{Cloud release version}-{? SNAPSHOT if it's a dev build}
-    define('APPLICATION_VERSION', 'bdd94ba7ec0938fd9a49817b0de60b9c3073ada4');
+    define('APPLICATION_VERSION', '84f01feabde5ecbb7b7d62f648a7659761dc8410');
 }
 if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);
